eaef827ef747e9d3d196a92c54846e4853c87782
himanshusoni <himanshuson@gmail.com>
2016-04-17 18:54:20 -0500

    Updated Event Details Page and Started Building Discussion page

--- app/src/main/java/com/hexade/borntoparty/main/UI/Activities/EventDetailActivity.java
+++ app/src/main/java/com/hexade/borntoparty/main/UI/Activities/EventDetailActivity.java
@@ -10,15 +10,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.hexade.borntoparty.main.R;
+import com.hexade.borntoparty.main.UI.Fragments.ItemDetailFragment;
+import com.hexade.borntoparty.main.models.BornToPartyUser;
+import com.hexade.borntoparty.main.models.Event;
+import com.hexade.borntoparty.main.models.Store;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class EventDetailActivity extends AppCompatActivity {
+
+    public static final String ARG_ITEM_ID = "item_id";
+    private Event mItem;
+
+    private TextView event_header;
+    private TextView invitesText;
+    private TextView activitiesText;
+
+    private TextView event_location;
+    private TextView event_organizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+
+        event_header = (TextView) findViewById(R.id.event_header);
+        invitesText = (TextView) findViewById(R.id.event_invites_info);
+        activitiesText = (TextView) findViewById(R.id.event_activities_info);
+        event_location = (TextView) findViewById(R.id.event_location);
+        event_organizer = (TextView) findViewById(R.id.event_organizer);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.event_detail_toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,10 +59,38 @@ public class EventDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                openDiscussion();
             }
         });
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle.containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            String eventId = bundle.getString(ARG_ITEM_ID);
+
+            ArrayList<Event> eventList = Store.getEventList();
+            for(Event f : eventList){
+                if(f.get_id().equals(eventId)){
+                    mItem = f;
+                    break;
+                }
+            }
+
+            if(mItem!=null){
+                event_header.setText(mItem.getEventName());
+                event_organizer.setText(mItem.getCreatedBy());
+                event_location.setText(mItem.getLocation());
+                invitesText.setText("Who's Coming\n"+mItem.getInvites().toString());
+                activitiesText.setText("Activities\n"+mItem.getActivities().toString());
+            }
+        }
     }
 
     @Override
@@ -53,6 +108,13 @@ public class EventDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openDiscussion(){
+        Intent intent = new Intent(this, DiscussionActivity.class);
+        intent.putExtra(ARG_ITEM_ID, mItem.get_id());
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
